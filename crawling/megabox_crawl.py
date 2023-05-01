16cbcb1234c625dd57e31c890f1cb2e675b62b82
hyedall <dahye991228@gmail.com>
2023-05-02 01:56:29 +0900

    크롤링 코드 완성본 - 메가박스는 자정에 들어갔을 때 해당 날짜가 뜨지 않아 다음 날로 눌러줘야 함

--- crawling/megabox_crawl.py
+++ crawling/megabox_crawl.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import datetime
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service #
 from webdriver_manager.chrome import ChromeDriverManager # 
 from selenium.webdriver.common.by import By
+
 
 
 def theater_crawl(result, theater_name, theater_name_url):
@@ -20,11 +22,14 @@ def theater_crawl(result, theater_name, theater_name_url):
     driver_detail.find_element(By.CSS_SELECTOR, '#contents > div.inner-wrap.pt40 > div.tab-list.fixed.mb40.tab-layer > ul > li:nth-child(2) > a').click()
     driver_detail.implicitly_wait(2)
 
+    # 12시에 들어갔을 때 아직 날짜가 안바뀐 것을 볼 수 있음 -> 다음 날로 눌러주기
+    driver_detail.find_element(By.CSS_SELECTOR, '#tab02 > div.time-schedule.mb30 > div > div.date-list > div.date-area > div > button:nth-child(3)').click()
+
     # 영화 제목별 ex) 슈퍼마리오
-    movie_elements = driver_detail.find_elements(By.CSS_SELECTOR, '#tab02 > div.reserve.theater-list-box > div')
+    movie_elements = driver_detail.find_elements(By.CSS_SELECTOR, '#tab02 > div.reserve.theater-list-box > div.theater-list')
     for movie_element in movie_elements:
         movie_title = movie_element.find_element(By.CSS_SELECTOR, 'div.theater-tit > p:nth-child(2) > a').text
-        start_time_elements = movie_elements.find_elements(By.CSS_SELECTOR , 'div.theater-type-box > div.theater-time > div.theater-time-box > table > tbody > tr > td > div > div.txt-center > a > p.time')
+        start_time_elements = movie_element.find_elements(By.CSS_SELECTOR , 'div.theater-type-box > div.theater-time > div.theater-time-box > table > tbody > tr > td > div > div.txt-center > a > p.time')
         for start_time_element in start_time_elements:
             # 상영 시각
             start_time = start_time_element.text
@@ -55,7 +60,7 @@ def location_crawl(driver, location_element):
         print(f'{theater_name} 완료')
     
     print(result)
-    movie_tbl = pd.DataFrame(result,  columns=('theater_type',  'theater_name', 'location', 'movie_title', 'start_time', 'run_time'))
+    movie_tbl = pd.DataFrame(result,  columns=('theater_type',  'theater_name', 'location', 'movie_title', 'start_time'))
     return movie_tbl
         
 def megabox_crawl():
@@ -74,9 +79,9 @@ def megabox_crawl():
     # 앞 3개만 크롤링 (서울, 경기, 인천) / 0, 1, 2
     location = {0:'서울', 1:'경기/인천', 2:'경기/인천'}
     for i in range(3):
-        # 테스트 
-        if i ==2 :
-            break
+        # # 테스트 
+        # if i ==1 :
+        #     break
 
         print(f'>>> {location[i]} 시작')
         movie_tbl = location_crawl(driver, location_elements[i])
@@ -88,11 +93,12 @@ def megabox_crawl():
     except:
         movie_total = movie_tbl
 
-    
-    movie_total.to_csv(f'/output_file/메가박스 상영시간_{datetime.datetime.now()}', encoding='utf-8', index = False )
+    WORKING_DIR = os.getcwd()
+    movie_total.to_csv(f'output_file/메가박스 상영시간_{datetime.datetime.now()}.csv', encoding='utf-8', index = False )
+    # movie_total.to_csv(os.path.join(WORKING_DIR, f'output_file/메가박스 상영시간_{datetime.datetime.now()}.csv'), encoding='utf-8', index = False )
     driver.quit()
 
-    
 
-megabox_crawl()
+if __name__ == '__main__':
+    megabox_crawl()
     
